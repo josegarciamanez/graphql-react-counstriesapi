4b7cfea9a093f7e187377379abe3dc31fca4b32d
José García Máñez <josegarciamanez@gmail.com>
2020-02-09 18:55:56 +0100

    finish first part of the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Graphql - React - CountriesAPI</h1>
+
+        <div class='container'>
+          <div className='row'>
+            {this.props.data.loading === true
+              ? 'Loading'
+              : this.props.data.countries.map(country => (
+                  <div class='col s12 m6 l4' key={country.name}>
+                    <div className='card'>
+                      <div className='card-image waves-effect waves-block waves-light center-align'>
+                        <i className='circle z-depth-1 flag activator'>
+                          {country.emoji}
+                        </i>
+                      </div>
+                      <div className='card-content'>
+                        <span className='card-title activator grey-text text-darken-4 truncate'>
+                          {country.name}
+                          <i className='material-icons right'>more_vert</i>
+                        </span>
+                      </div>
+                      <div className='card-reveal teal  accent-3'>
+                        <span className='card-title grey-text text-darken-4'>
+                          {country.name}
+                          <i className='material-icons right'>close</i>
+                        </span>
+                        <p className='white-text'>{country.continent.name}</p>
+                        <p className='white-text'>Moneda: {country.currency}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const repoQuery = gql`
+  query {
+    countries {
+      name
+      code
+      native
+      currency
+      emoji
+      emojiU
+      languages {
+        name
+        native
+        code
+      }
+      continent {
+        name
+      }
+    }
+  }
+`;
+
+const AppWithData = graphql(repoQuery, {
+  options: {
+    variables: {
+      // name: 'medellinjs'
+    }
+  }
+})(App);
+
+export default AppWithData;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const token = 'YOUR_TOKEN';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const httpLink = {
+  uri: 'https://countries.trevorblades.com/',
+  headers: {
+    authorization: `Bearer ${token}`
+  }
+};
+
+const client = new ApolloClient({
+  link: new HttpLink(httpLink),
+  cache: new InMemoryCache()
+});
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
 serviceWorker.unregister();
